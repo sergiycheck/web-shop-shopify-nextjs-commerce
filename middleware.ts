65b72888473c48d8a6cce5ccffe42dda8b16aa36
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-05-01 01:45:58 +0300

    feat: add locale and redirect

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+
+const locales = ['en-US', 'uk-UA', 'uk'];
+const defaultLocale = 'uk-UA';
+
+function getLocale(request: NextRequest) {
+  const headers = request.headers;
+  const languages = new Negotiator({ headers }).languages();
+
+  // return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  );
+
+  if (pathnameHasLocale) return;
+
+  const locale = 'uk';
+
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    '/((?!_next).*)'
+    // Optional: only run on root (/) URL
+    // '/'
+  ]
+};
