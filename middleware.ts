183a374089748ee6b028f8f5287fe7ff5df5d6da
Serhii Kuzmuch <sergiycheck@gmail.com>
2024-05-01 14:59:08 +0300

    fix: redirect to uk locale by default

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,7 @@
 import { NextResponse, NextRequest } from 'next/server';
-// import { match } from '@formatjs/intl-localematcher';
-// import Negotiator from 'negotiator';
-
-const locales = ['en-US', 'uk-UA', 'uk'];
-// const defaultLocale = 'uk-UA';
-
-// function getLocale(request: NextRequest) {
-//   const headers = request.headers;
-//   const languages = new Negotiator({ headers }).languages();
-
-//   // return match(languages, locales, defaultLocale);
-// }
 
 export function middleware(request: NextRequest) {
-  console.log(request.url);
-
   const { pathname } = request.nextUrl;
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
-
-  if (pathnameHasLocale) return;
 
   const locale = 'uk';
 
